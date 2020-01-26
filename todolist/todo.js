c828e12bf00f225042a70a83ce8b41ba96d9f51b
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-01-26 01:35:46 +0000

    A todo list that allows you to check and uncheck items. Unable to add or remove items from list

---
+++ todolist/todo.js
@@ -0,0 +1,80 @@
+var firstThing = document.getElementById("one");
+var secondThing = document.getElementById("two");
+var thirdThing = document.getElementById("three");
+var fourthThing = document.getElementById("four");
+var fifthThing = document.getElementById("five");
+var sixthThing = document.getElementById("six");
+
+
+
+firstThing.addEventListener("mouseover", function(){
+    firstThing.style.color="pink";
+});
+
+firstThing.addEventListener("mouseout", function(){
+    firstThing.style.color="black";
+});
+
+firstThing.addEventListener("click", function(){
+    firstThing.classList.toggle("done");
+});
+
+secondThing.addEventListener("mouseover", function(){
+    secondThing.style.color="pink";
+});
+
+secondThing.addEventListener("mouseout", function(){
+    secondThing.style.color="black";
+});
+
+secondThing.addEventListener("click", function(){
+    secondThing.classList.toggle("done");
+});
+
+thirdThing.addEventListener("mouseover", function(){
+    thirdThing.style.color="pink";
+});
+
+thirdThing.addEventListener("mouseout", function(){
+    thirdThing.style.color="black";
+});
+
+thirdThing.addEventListener("click", function(){
+    thirdThing.classList.toggle("done");
+});
+
+fourthThing.addEventListener("mouseover", function(){
+    fourthThing.style.color="pink";
+});
+
+fourthThing.addEventListener("mouseout", function(){
+    fourthThing.style.color="black";
+});
+
+fourthThing.addEventListener("click", function(){
+    fourthThing.classList.toggle("done");
+});
+
+fifthThing.addEventListener("mouseover", function(){
+    fifthThing.style.color="pink";
+});
+
+fifthThing.addEventListener("mouseout", function(){
+    fifthThing.style.color="black";
+});
+
+fifthThing.addEventListener("click", function(){
+    fifthThing.classList.toggle("done");
+});
+
+sixthThing.addEventListener("mouseover", function(){
+    sixthThing.style.color="pink";
+});
+
+sixthThing.addEventListener("mouseout", function(){
+    sixthThing.style.color="black";
+});
+
+sixthThing.addEventListener("click", function(){
+    sixthThing.classList.toggle("done");
+});
